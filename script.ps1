6f77a67807559d1b263cb2d01371460c99a1180e
Lucas Rausch <lucas.rausch.732@k12.friscoisd.org>
2024-02-03 15:50:28 -0600

    ADD `| Out-Null` TO script.ps1

--- script.ps1
+++ script.ps1
@@ -5,17 +5,17 @@ Write-Host "Installs: NVCleanstall, Steam, Discord, Firefox, Malwarebytes, Tor, 
 Write-Host "(Last Update, 2/3/2024)";
 Write-Host "Them: Imagine not having java 8!!!!!!! i'm so angy bc not java 8 | Us: install it yourself kekw";
 Write-Host "----------------------";
-winget install -e --id TechPowerUp.NVCleanstall
-winget install -e --id Valve.Steam
-winget install -e --id Discord.Discord
-winget install -e --id Mozilla.Firefox
-winget install -e --id Malwarebytes.Malwarebytes
-winget install -e --id TorProject.TorBrowser
-winget install -e --id qBittorrent.qBittorrent
-winget install -e --id Rclone.Rclone
-winget install -e --id DominikReichl.KeePass
-winget install -e --id Git.Git
-winget install -e --id PrismLauncher.PrismLauncher
-winget install -e --id VideoLAN.VLC
-winget install -e --id Oracle.VirtualBox
-winget install -e --id AdoptOpenJDK.OpenJDK.17
+winget install -e --id TechPowerUp.NVCleanstall | Out-Null
+winget install -e --id Valve.Steam | Out-Null
+winget install -e --id Discord.Discord | Out-Null
+winget install -e --id Mozilla.Firefox | Out-Null
+winget install -e --id Malwarebytes.Malwarebytes | Out-Null
+winget install -e --id TorProject.TorBrowser | Out-Null
+winget install -e --id qBittorrent.qBittorrent | Out-Null
+winget install -e --id Rclone.Rclone | Out-Null
+winget install -e --id DominikReichl.KeePass | Out-Null
+winget install -e --id Git.Git | Out-Null
+winget install -e --id PrismLauncher.PrismLauncher | Out-Null
+winget install -e --id VideoLAN.VLC | Out-Null
+winget install -e --id Oracle.VirtualBox | Out-Null
+winget install -e --id AdoptOpenJDK.OpenJDK.17 | Out-Null
