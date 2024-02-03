77e998a0c5ad285fc58386ef5f7622314e6f4b57
Lucas Rausch <lucas.rausch.732@k12.friscoisd.org>
2024-02-03 15:34:57 -0600

    Create script.ps1

---
+++ script.ps1
@@ -0,0 +1,21 @@
+"The Winget Install Bundle! (or CAUTOPROV596)"
+"Version 0.5.96"
+"List Curated by @bakodoesyt and @Central596 on youtube.com"
+"Installs: NVCleanstall, Steam, Discord, Firefox, Malwarebytes, Tor, qBitTorrent, Rclone, VLC, Prism Launcher, Virtualbox, and JDK 17."
+"(Last Update, 2/3/2024)"
+"Them: Imagine not having java 8!!!!!!! i'm so angy bc not java 8 | Us: install it yourself kekw"
+"----------------------"
+winget install -e --id TechPowerUp.NVCleanstall
+winget install -e --id Valve.Steam
+winget install -e --id Discord.Discord
+winget install -e --id Mozilla.Firefox
+winget install -e --id Malwarebytes.Malwarebytes
+winget install -e --id TorProject.TorBrowser
+winget install -e --id qBittorrent.qBittorrent
+winget install -e --id Rclone.Rclone
+winget install -e --id DominikReichl.KeePass
+winget install -e --id Git.Git
+winget install -e --id PrismLauncher.PrismLauncher
+winget install -e --id VideoLAN.VLC
+winget install -e --id Oracle.VirtualBox
+winget install -e --id AdoptOpenJDK.OpenJDK.17
